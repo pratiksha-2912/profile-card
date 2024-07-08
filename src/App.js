7916ea75e745cfc6585d3c230c3078dffa5812b6
Pratiksha Shende <pratiksha.shende@ril.com>
2024-07-08 19:28:30 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+export default function App() {
+return(
+  <div className="profile-card">
+  <Avtar/>
+  <ProfileCard/>
+  </div>
+)
+}
+function Avtar(){
+  return <img className="avatar" src= "p.jpg" alt="pratiksha"/>
+}
+
+function ProfileCard() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="profile-card">
+      <div className="profile-info">
+        <h2>Pratiksha</h2>
+        <p>A brief description about yourself goes here. You can talk about your experience, interests, or any other details you find relevant.</p>
+        <div className="technologies">
+          <span>JavaScript</span>
+          <span>React</span>
+          <span>CSS</span>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+ 
